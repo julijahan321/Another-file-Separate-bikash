2a0d8cec021b048c48e96edd093f41f355e1db34
juli-jaan <julijahan.tondra321@gmail.com>
2023-07-19 20:39:05 +0600

    This is the first commit for bikash separate file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class program
+{
+        public static void Main()
+        {
+            while (true)
+            {
+                Bikash bikash = new Bikash();
+                
+                string inputNumber1 = Convert.ToString(Console.ReadLine());
+                bikash.BikashSystem(inputNumber1);
+
+                Recharge recharge = new Recharge();
+
+                recharge.RechergeMoney();
+            }
+        }
+    }
